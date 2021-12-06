53d26ca8b4418bece13e05eed9b5e41689b2efb2
CYBERdanya2003 <danya.efimov0@yandex.ru>
2021-12-06 05:52:47 +0500

    hw1

--- 1ksat/1ksat/Program.cs
+++ 1ksat/1ksat/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _1ksat
 {
@@ -10,6 +6,30 @@ namespace _1ksat
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Владимир Маяковский 'Горе'");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"Тщетно отчаянный ветер
+ился нечеловече
+Капли чернеющей крови
+стынут крышами кровель.
+И овдовевшная в ночи
+вышла луна одиночить");
+            Console.ReadKey();
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Владимир Маяковский 'Горе'");
+                Console.WriteLine();
+                Console.WriteLine(@"Тщетно отчаянный ветер
+ился нечеловече
+Капли чернеющей крови
+стынут крышами кровель.
+И овдовевшная в ночи
+вышла луна одиночить");
+                Console.ReadKey();
+            }
         }
     }
 }
